4b0950a80402a7de80547f14f756fe74d9ed9f2a
Joel-HackMaster <u21216300@utp.edu.pe>
2025-11-29 22:12:04 -0500

    Actualizacion arduino

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,12 @@
 
 #define DHTTYPE DHT11
 #define DHTPIN 9
+#define FAN_PIN 3
 
 DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long previousMillis = 0;
+const long interval = 2000;
 
 void setup() {
   Serial.begin(9600);
@@ -12,19 +16,36 @@ void setup() {
 }
 
 void loop() {
-  delay(2000);
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
+  unsigned long currentMillis = millis();
 
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+  if (currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (!isnan(h) && !isnan(t)) {
+      Serial.print("{\"temp\": ");
+      Serial.print(t);
+      Serial.print(", \"hum\": ");
+      Serial.print(h);
+      Serial.println("}");
+    }
   }
+  
+  if(Serial.available() > 0){
+    String command = Serial.readStringUntil('\n');
+    command.trim();
 
-  Serial.print("Temperatura: ");
-  Serial.print(temperature);
-  Serial.println(" °C");
-  Serial.print("Humedad: ");
-  Serial.print(humidity);
-  Serial.println(" %");
+    if(command.startsWith("VOLT:")){
+      String valStr = command.substring(5);
+      float voltage = valStr.toFloat();
+
+      if (voltage > 5.0) voltage = 5.0;
+      if (voltage < 0.0) voltage = 0.0;
+
+      int pwmValue = (voltage / 5.0) * 255;
+      
+      analogWrite(FAN_PIN, pwmValue);
+    }
+  }
 }
